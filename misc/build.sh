6bbda64fda16c40804a5b98ec494ef13f390f9d0
Rasmus Andersson <rasmus@notion.se>
2019-11-17 16:28:19 -0800

    build script: fix comments

--- misc/build.sh
+++ misc/build.sh
@@ -29,11 +29,30 @@ chmod +x wasmc.g
 
 if ! $DEBUG; then
 
+# strip comments
+node <<_JS
+let fs = require('fs')
+let s = fs.readFileSync("wasmc.g", "utf8")
+// replace with whitespace and linebreaks to not mess up sourcemap
+s = s.replace(/(?:^|\n\s*)\/\*(?:(?!\*\/).)*\*\//gms, s => {
+  let s2 = ""
+  for (let i = 0; i < s.length; i++) {
+    if (s.charCodeAt(i) == 0xA) {
+      s2 += "\n"
+    } else {
+      s2 += " "
+    }
+  }
+  return s2
+})
+fs.writeFileSync(".wasmc.o1", s, "utf8")
+_JS
+
   CCOMPILER=$(node -e "process.stdout.write(require('google-closure-compiler/lib/utils').getNativeImagePath())")
   echo "running closure-compiler"
   $CCOMPILER \
     -O=SIMPLE \
-    --js=wasmc.g \
+    --js=.wasmc.o1 \
     --js_output_file=wasmc \
     --language_in=ECMASCRIPT_2018 \
     --language_out=ECMASCRIPT_2018 \
@@ -43,16 +62,11 @@ if ! $DEBUG; then
     --package_json_entry_names=esnext:main,browser,main \
     --assume_function_wrapper \
     --create_source_map=wasmc.map \
-    --source_map_input="wasmc.g|wasmc.g.map" \
+    --source_map_input=".wasmc.o1|wasmc.g.map" \
     \
     --charset=UTF-8 \
     --output_wrapper="$(printf "#!/usr/bin/env node\n%%output%%\n//#sourceMappingURL=wasmc.map")"
 
-  # uglifyjs --compress --mangle --ecma 7 -o wasmc --source-map \
-  #   content=wasmc.g.js.map,includeSources=false \
-  #   -- \
-  #   wasmc.g.js
-
-  # node misc/fixup-sourcemap.js wasmc.map
+  rm .wasmc.o1
   chmod +x wasmc
 fi
