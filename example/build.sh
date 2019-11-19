37d7083b67af22dd4a90e905779cfcfe62c7657b
Rasmus Andersson <rasmus@notion.se>
2019-11-18 16:13:02 -0800

    example

--- example/build.sh
+++ example/build.sh
@@ -12,6 +12,7 @@ set -e
 cd "$(dirname "$0")"
 
 if [ "$1" == "-local" ]; then
+  mkdir -p out
 
   # compile C to WASM
   echo "emcc" *.c "-> out/.tmp/foo.js"
@@ -23,20 +24,21 @@ if [ "$1" == "-local" ]; then
     -s ABORTING_MALLOC=0 \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s DISABLE_EXCEPTION_CATCHING=1 \
+    -s ERROR_ON_UNDEFINED_SYMBOLS=1 \
     *.c -o out/foo.js
 
   cp -a out/foo.js out/emcc.foo.js
   cp -a out/foo.wasm out/emcc.foo.wasm
 
   # Bundle, combining your javascript and wasm code
-  echo "wasmc out/foo.js foo.js -> out/foo.js"
-  ../wasmc.g -syncinit out/foo.js foo.js
+  echo "wasmc -syncinit out/foo.js foo.js"
+  ../wasmc -syncinit out/foo.js foo.js
 
   # Run via nodejs
   echo 'Testing in nodejs: require("./out/foo.js").hello()'
   node -e 'require("./out/foo.js").hello()'
 
-  # if we did not provide -embed=sync then we'd have to wait for the
+  # if we did not provide -syncinit then we'd have to wait for the
   # "ready" promise before calling functions:
   # echo 'Testing in nodejs via require("./out/foo.js")'
   # node -e 'require("./out/foo.js").ready.then(m => m.hello())'
