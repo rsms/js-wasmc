bd0c7aaa0f79eaa1c981a95e6849b00ae7e0b844
Michael Bullington <michael@bullington.xyz>
2021-03-19 21:38:45 -0400

    switch to use -docker-image and rebuild

--- src/cmd_build.js
+++ src/cmd_build.js
@@ -11,7 +11,7 @@ export async function cmd_build(c, opts, args) {
   c.watch = opts.watch || opts.w
   c.force = opts.clean
   c.quiet = opts.quiet || opts.q
-  c.image = opts.image
+  c.image = opts['docker-image'] || opts.dockerImage
 
   // configure
   dlog(">> configure")
--- src/main.js
+++ src/main.js
@@ -30,15 +30,15 @@ wasmc ${WASMC_VERSION} WebAssembly builder.
 usage: wasmc [options] [<dir>]
 usage: wasmc [-C <dir>] -T<tool> [<tool-arg> ...]
 options:
-  -debug, -g       Disable optimizations and include data for debuggers.
-  -watch, -w       Watch source files and rebuild as needed.
-  -config <file>   Load config file from <file> instead of <dir>/wasmc.js
-  -image <string>  Docker image to build with instead of ${defaultEmsdkDockerImage}
-  -clean           Rebuild even when product and sources are up to date.
-  -quiet, -q       Do not print information to stdout except for warnings and errors
-  -help, -h        Show help message and exit
-  -C <dir>         Change working directory; as if wasmc was invoked from <dir>.
-  -T<tool>         Run <tool> instead of building. -T for list of tools.
+ -debug, -g              Disable optimizations and include data for debuggers.
+ -watch, -w              Watch source files and rebuild as needed.
+ -config <file>          Load config file from <file> instead of <dir>/wasmc.js
+ -docker-image <string>  Docker image to build with instead of rsms/emsdk:1.39.4
+ -clean                  Rebuild even when product and sources are up to date.
+ -quiet, -q              Do not print information to stdout except for warnings and errors
+ -help, -h               Show help message and exit
+ -C <dir>                Change working directory; as if wasmc was invoked from <dir>.
+ -T<tool>                Run <tool> instead of building. -T for list of tools. 
 
 <dir>
   The module directory. Defaults to "." (dirname(<file>) with -config)
